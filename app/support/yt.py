398f6bc444e54ff32558e719c862532a0bd2b765
D101028 <104710811+D101028@users.noreply.github.com>
2026-03-22 10:55:49 +0800

    add cookies verification for yt-dlp

--- app/support/yt.py
+++ app/support/yt.py
@@ -99,6 +99,7 @@ def yt_download_audio(vid: str, basename: str, dest: str, ext: str = "mp3"):
            "--ffmpeg-location", Config.FFMPEG_LOCATION, 
            "--extract-audio", 
            "-o", "temp", 
+           "--cookies", "cookies.txt", 
            url]
     process = Popen(cmd, cwd=temp_dir)
     status = process.wait()
